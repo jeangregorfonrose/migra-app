0a2dcb8d8e34ababb11d3054aeb3c20b7e04bd48
Jean Gregor Fonrose <fonrosejeangregor@gmail.com>
2025-10-04 22:35:11 -0400

    Replace strings with constants

--- lib/core/utils/location.dart
+++ lib/core/utils/location.dart
@@ -1,4 +1,5 @@
 import 'package:geolocator/geolocator.dart';
+import 'package:migra_app/core/utils/constants.dart';
 
 Future<Position> getUserLocation() async {
   bool serviceEnabled;
@@ -10,7 +11,7 @@ Future<Position> getUserLocation() async {
     // Location services are not enabled don't continue
     // accessing the position and request users of the
     // App to enable the location services.
-    return Future.error('Location services are disabled.');
+    return Future.error(AppConstants.locationServicesDisabled);
   }
 
   // Check for location permissions.
@@ -23,14 +24,13 @@ Future<Position> getUserLocation() async {
       // Android's shouldShowRequestPermissionRationale
       // returned true. According to Android guidelines
       // your App should show an explanatory UI now.
-      return Future.error('Location permissions are denied');
+      return Future.error(AppConstants.locationPermissionDenied);
     }
   }
 
   if (permission == LocationPermission.deniedForever) {
     // Permissions are denied forever, handle appropriately.
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request permissions.');
+    return Future.error(AppConstants.locationPermissionDeniedForever);
   }
 
   // When we reach here, permissions are granted and we can
