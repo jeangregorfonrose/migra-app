e54e355c3684ca9d0cb2886bc4843dd546344f90
Jean Gregor Fonrose <fonrosejeangregor@gmail.com>
2025-10-05 19:23:55 -0400

    Added api class

---
+++ lib/api/api_client.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart' as http;
+import 'package:migra_app/core/utils/constants.dart';
+
+class ApiClient {
+  static const String baseUrl = AppConstants.apiBaseUrl;
+
+  final http.Client _http;
+
+  ApiClient(this._http);
+
+  Future<http.Response> get(String path) async {
+    final uri = Uri.parse('$baseUrl$path');
+    return await _http.get(uri, headers: _headers());
+  }
+
+  Future<http.Response> post (String path, {Object? body}) async {
+    final uri = Uri.parse('$baseUrl$path');
+    return await _http.post(uri, headers: _headers(), body: body);
+  }
+
+  Map<String, String> _headers() {
+    return {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    // add Auth token if needed
+  };
+}
