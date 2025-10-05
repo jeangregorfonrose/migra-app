6664b2f2af6c46806ef09349be0f625770395965
Jean Gregor Fonrose <fonrosejeangregor@gmail.com>
2025-10-04 22:34:41 -0400

    Added constant class

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  AppConstants._(); // Private constructor to prevent instantiation
+
+  static const String appName = 'Migra App';
+  static const String apiBaseUrl = 'update_with_your_api_url';
+  
+
+  // Error messages
+  static const String locationServicesDisabled = 'Location services are disabled.';
+  static const String locationPermissionDenied = 'Location permissions are denied';
+  static const String locationPermissionDeniedForever = 'Location permissions are permanently denied, we cannot request permissions.';
+}
